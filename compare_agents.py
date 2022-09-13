d6632a7c6b7534c7b01807dbe9f052379b560575
TMat <timothee.mathieu@inria.fr>
2022-09-13 13:39:07 +0200

    remove ranks

--- compare_agents.py
+++ compare_agents.py
@@ -28,6 +28,8 @@ class MultipleAgentsComparator:
 
     For now, implement only a two-sided test.
 
+    !! STILL NOT FINISHED !! For now, use TwoAgentsComparator
+
     Parameters
     ----------
 
@@ -42,9 +44,6 @@ class MultipleAgentsComparator:
 
     name: str in {'PK', 'OF'}, default = "PK"
         type of spending function to use.
-
-    ttype: str in {'rank', 'mean'}
-        type of test used. rank is the default, it is more robust and less variable. 
 
     n_evaluations: int, default=10
         number of evaluations used in the function _get_rewards.
@@ -61,7 +60,7 @@ class MultipleAgentsComparator:
         p-value of the test
     """
 
-    def __init__(self, n=5, K=5, alpha=0.05, name="PK", ttype="rank", n_evaluations=1, seed=None):
+    def __init__(self, n=5, K=5, alpha=0.05, name="PK",  n_evaluations=1, seed=None):
         self.n = n
         self.K = K
         self.alpha = alpha
@@ -85,7 +84,7 @@ class MultipleAgentsComparator:
 
         decisions = []
         for pair in pairs:
-            comparator = Two_AgentsComparator(self.n, self.K, self.alpha, self.name, self.ttype, self.n_evaluations, self.seed)
+            comparator = Two_AgentsComparator(self.n, self.K, self.alpha, self.name, self.n_evaluations, self.seed)
             comparator.compare(pair[0], pair[1])
             decisions.append((comparator.agent1_name, comparator.agent2_name), comparator.decision)
         
@@ -114,9 +113,6 @@ class Two_AgentsComparator:
     name: str in {'PK', 'OF'}, default = "PK"
         type of spending function to use.
 
-    ttype: str in {'rank', 'mean'}
-        type of test used. rank is the default, it is more robust and less variable. 
-
     n_evaluations: int, default=10
         number of evaluations used in the function _get_rewards.
 
@@ -129,12 +125,11 @@ class Two_AgentsComparator:
         decision of the test.
     """
 
-    def __init__(self, n=5, K=5, alpha=0.05, name="PK", ttype="rank", n_evaluations=1, seed=None):
+    def __init__(self, n=5, K=5, alpha=0.05, name="PK",  n_evaluations=1, seed=None):
         self.n = n
         self.K = K
         self.alpha = alpha
         self.name = name
-        self.ttype = ttype
         self.n_evaluations = n_evaluations
         self.boundary = []
         self.test_stats = []
@@ -144,6 +139,9 @@ class Two_AgentsComparator:
         self._writer = DefaultWriter("Comparator")
 
     def get_spending_fun(self):
+        """
+        Return the spending function corresponding to self.name
+        """
         if self.name == "PK":
             return lambda p: self.alpha * np.log(1 + np.exp(1) * p - p)
         elif self.name == "OF":
@@ -155,6 +153,9 @@ class Two_AgentsComparator:
 
 
     def compute_means_diffs(self,  k, Z, boundary):
+        """
+        Compute the means differences.
+        """
         Zk = Z[(k*2*self.n):((k+1)*2*self.n)]
         ids_pos = itertools.combinations(np.arange(2*self.n), self.n)
         id_md = 0
@@ -179,84 +180,8 @@ class Two_AgentsComparator:
         # very inefficient. To do : improve computation.
             
         return self.mean_diffs
-        
-    def explore_graph_ranks(self, k, Rs, boundary):
-        """
-        Explore graph of permutations. Used to get the boundary
+    
 
-        Parameters
-        ----------
-        k: int
-            current interim in the algorithm
-        Rs: array of arrays
-            ranks or values of the data until now, at each interim
-        boundary: list of floats
-            boundary until now
-        """
-        sl = SortedList()
-        sl.add([0] * (k + 1))
-        records = (
-            sl,
-            [1],
-            [(0, 0)],
-        )  # rank score, count of number of path yielding rank, nodes
-        for j in range(k + 1):
-            records = (records[0], records[1], [(0, 0)] * len(records[0]))
-            for f in range(2 * self.n):  # Stage f of the network
-                old_records = (records[0], records[1], records[2])
-                records = (SortedList(), [], [])
-                for id_record in range(len(old_records[0])):  # Step 1
-                    u = old_records[0][id_record]
-                    cu = old_records[1][id_record]
-                    c = old_records[2][id_record]
-                    children = _get_children(c, 2 * self.n)  # Step 2
-                    if children is not None:
-                        for child in children:
-                            unew = copy(u)  # Step 3
-                            for l in range(k + 1 - j):
-                                unew[l] = unew[l] + Rs[j + l][f + 2 * self.n * j] * (
-                                    child[1] - c[1]
-                                )
-
-                            if len(records[0]) > 0:
-                                is_in = unew in records[0]
-                            else:
-                                is_in = False
-                            if is_in:
-                                l = records[0].index(unew)
-
-                            if np.any(is_in) and (records[2][l] == child):  # Step 4
-                                records[1][l] += cu
-                            else:
-                                records[0].add(unew)
-                                l = records[0].index(unew)
-                                records[1].insert(l, cu)
-                                records[2].insert(l, child)
-            if j != k:
-                records = self.postprocess_records(records, boundary, j)
-
-        return records
-
-
-
-    def postprocess_records(self, records, boundary, j):
-        # Pruning for conditional proba
-        idxs = np.where(np.array([boundary[j][0] <= r[0] <= boundary[j][1] for r in records[0]]))[0]
-        if len(idxs) > 0:
-            new_list = [records[0][i] for i in idxs]
-            records = (
-                new_list,
-                list(np.array(records[1])[idxs]),
-                list(np.array(records[2])[idxs]),
-            )
-        else:
-            records = (list(records[0]), records[1], records[2])
-
-        # Remove the first element of all the records.
-        for i in range(len(records[0])):
-            records[0][i] = records[0][i][1:]
-        # returns unsorted list as we don't need sorted lists for old_records
-        return records
 
     def partial_compare(self, Z, X, k):
         """
@@ -287,16 +212,8 @@ class Two_AgentsComparator:
         clevel = spending_fun((k + 1) / self.K)
 
 
-        if self.ttype == "rank":
-            Rs = self._get_ranks(Z, k)
-        if self.ttype == "rank":
-            records = self.explore_graph_ranks(k, Rs, self.boundary)
-            rs = np.array(records[0]).ravel()
-            probas = np.array(records[1]) / binom(2 * self.n, self.n) ** (k + 1)
-
-        else:
-            rs = self.compute_means_diffs( k, Z, self.boundary)
-            probas = np.ones(len(rs)) / len(rs)
+        rs = self.compute_means_diffs( k, Z, self.boundary)
+        probas = np.ones(len(rs)) / len(rs)
             
         idx = np.argsort(rs)
         values = np.array(rs)[idx]
@@ -324,10 +241,7 @@ class Two_AgentsComparator:
             level_to_add2 = 0
         assert bk_inf <= bk_sup
             
-        if self.ttype =='rank':
-            T = np.sum(Rs[-1] * X)
-        else:
-            T = np.sum(Z*(-1)**X)
+        T = np.sum(Z*(-1)**X)
             
         self.test_stats.append(T)
         
@@ -384,8 +298,7 @@ class Two_AgentsComparator:
         self.level_spent1 = 0
         self.level_spent2 = 0
 
-        if self.ttype != "rank":
-            self.mean_diffs = [0]
+        self.mean_diffs = [0]
        
         seeders = self.seeder.spawn(2*self.K)
         for k in range(self.K):
@@ -425,11 +338,6 @@ class Two_AgentsComparator:
         self.p_val = p_val
         logger.info('p value is '+str(p_val))
 
-    def _get_ranks(self, Z, k):
-        Rs = []
-        for j in range(k + 1):
-            Rs.append(rankdata(Z[: (2 * self.n * (j + 1))]))
-        return Rs
 
     def _get_evals(self, manager):
         """
@@ -445,19 +353,3 @@ class Two_AgentsComparator:
             )
         return eval_values
 
-
-def _get_children(c, nmax):
-    """
-    c is a couple (total size, size of assigned to 1)
-    nmax is int, maximum size
-    """
-    if c[0] == nmax:
-        return None
-    if c[1] == nmax // 2:
-        return [(c[0] + 1, c[1])]
-    elif c[0] - c[1] == nmax // 2:
-        return [(c[0] + 1, c[1] + 1)]
-    elif c[0] < nmax:
-        return [(c[0] + 1, c[1]), (c[0] + 1, c[1] + 1)]
-    else:
-        return None
