0de9501262a2c6ae105730409ab4872b5c60bd3b
KohlerHECTOR <hector.kohler@etu.sorbonne-universite.fr>
2022-11-07 00:16:09 +0100

    do plots

--- code/example_tabular_rl.py
+++ code/example_tabular_rl.py
@@ -8,7 +8,7 @@ manager1 = (
     QLAgent,
     dict(
         train_env=(TwentyOneWithDice, {}),
-        fit_budget=int(1e4),
+        fit_budget=int(3e5),
         agent_name="QL",
     ),
 )
@@ -16,38 +16,25 @@ manager2 = (
     SARSAAgent,
     dict(
         train_env=(TwentyOneWithDice, {}),
-        fit_budget=int(1e4),
+        fit_budget=int(3e5),
         agent_name="Sarsa",
     ),
 )
 
 managers = [manager1, manager2]
 
-n = 10
-K = 10
+n = 5
+K = 4
 B=100_000
 alpha = 0.05
 n_managers = len(managers)
 print('With these parameters, we have a maximum of {} fits done for each agent'.format(n*K))
 print('Number of comparisons is {}'.format(n_managers*(n_managers-1)/2))
 
-class Comparator(MultipleAgentsComparator):
-    def __init__(self, **kwargs):
-        MultipleAgentsComparator.__init__(self, **kwargs)
-
-    def _get_evals(self, manager):
-        """
-        Compute the cumulative reward.
-        """
-        eval_values = []
-        for idx in  manager.get_writer_data():
-            df = manager.get_writer_data()[idx]
-            eval_values.append(np.sum(df.loc[df['tag']=='reward', 'value']))
-        return eval_values
-
-comparator = Comparator(n=n, K=K, B=B, alpha=alpha, n_evaluations = 100)
+comparator = MultipleAgentsComparator(n=n, K=K, B=B, alpha=alpha, n_evaluations = 100)
 comparator.compare(managers)
 plt.boxplot(comparator.eval_values, labels=['QL', 'SARSA'])
-plt.show()
+plt.savefig("evals")
 print(comparator.mean_eval_values)
 comparator.plot_boundary()
+plt.savefig("compare")
