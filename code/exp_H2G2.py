fe083299afe54f55b3e09a239304d69cdb844a96
KohlerHECTOR <hector.kohler@etu.sorbonne-universite.fr>
2022-12-04 17:10:51 +0100

    The result is the bootstrapped estim of mean of nb of fits required.

---
+++ code/exp_H2G2.py
@@ -0,0 +1,29 @@
+import numpy as np
+from compare_agents import Two_AgentsComparator
+import matplotlib.pyplot as plt
+import seaborn as sns
+ks = []
+scalar_list1 = np.loadtxt("evals_sac_hc.txt")
+scalar_list2 = np.loadtxt("evals_td3_hc.txt")
+sns.set_style("whitegrid")
+sns.kdeplot(scalar_list1, x = "evaluation means", label = "SAC")
+sns.kdeplot(scalar_list2, x = "evaluation means", label  = "TD3")
+plt.xlabel("evaluations means")
+plt.legend()
+plt.savefig("sac_vs_td3_hc.png")
+
+res = np.zeros((5, 5))
+for n in range(1,6):
+    for k in range(1,6):
+        avg = []
+        for exps in range(50):
+            comparator = Two_AgentsComparator(B = 100_000, K = k,n = n)
+
+            scal1 = np.random.choice(scalar_list1, size = len(scalar_list1), replace = True)
+            scal2 = np.random.choice(scalar_list2, size = len(scalar_list1), replace = True)
+
+            comparator.compare_scalars(scal1, scal2)
+            avg.append(comparator.n_iter / 2)
+        res[n-1,k-1]=np.array(avg).mean()
+
+np.save("table_res.npy", res )
