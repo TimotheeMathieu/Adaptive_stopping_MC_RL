f659a66b7ff246645a32deae335c70f993cc3ab7
KohlerHECTOR <hector.kohler@etu.sorbonne-universite.fr>
2023-02-09 17:28:33 +0100

    updated code to save attributes dict

--- code/exp_H2G2.py
+++ code/exp_H2G2.py
@@ -2,30 +2,27 @@ import numpy as np
 from compare_agents import Two_AgentsComparator
 import matplotlib.pyplot as plt
 import seaborn as sns
-ks = []
-scalar_list1 = np.loadtxt("evals_sac_hc.txt")
-scalar_list2 = np.loadtxt("evals_td3_hc.txt")
-sns.set_style("whitegrid")
-sns.kdeplot(scalar_list1, x = "evaluation means", label = "SAC")
-sns.kdeplot(scalar_list2, x = "evaluation means", label  = "TD3")
-plt.xlabel("evaluations means")
-plt.legend()
-plt.savefig("sac_vs_td3_hc.png")
-all_comparator_stuff = []
-res = np.zeros((5, 5))
-for n in range(1,6):
-    for k in range(1,6):
-        avg = []
-        for exps in range(100):
-            comparator = Two_AgentsComparator(B = 100_000, K = k,n = n)
+import pickle
 
+# ks = []
+scalar_list1 = np.loadtxt("h2g2/evals_sac_hc.txt")
+scalar_list2 = np.loadtxt("h2g2/evals_td3_hc.txt")
+# sns.set_style("whitegrid")
+# sns.kdeplot(scalar_list1, x = "evaluation means", label = "SAC")
+# sns.kdeplot(scalar_list2, x = "evaluation means", label  = "TD3")
+# plt.xlabel("evaluations means")
+# plt.legend()
+# plt.savefig("h2g2/sac_vs_td3_hc.png")
+# all_comparator_stuff = []
+combs = [[3, 7], [4, 5], [4, 4], [2, 10]]
+for comb in combs:
+    for i in range(2):
+        n, k = comb[i], comb[1-i]
+        for exps in range(100):
+            filename = "h2g2/n-{}-k-{}-rep-{}.pkl".format(n,k,exps)
+            comparator = Two_AgentsComparator(B = 100_000, K = k,n = n)
             scal1 = np.random.choice(scalar_list1, size = len(scalar_list1), replace = True)
             scal2 = np.random.choice(scalar_list2, size = len(scalar_list1), replace = True)
-
-            stuff_comparator = comparator.compare_scalars(scal1, scal2)
-            all_comparator_stuff.append(stuff_comparator)
-            avg.append(comparator.n_iter / 2)
-        res[n-1,k-1]=np.array(avg).mean()
-
-np.save("table_res.npy", res )
-np.save("results_comparator.npy", all_comparator_stuff)
+            comparator.compare_scalars(scal1, scal2)
+            with open(filename, "wb") as f:
+                pickle.dump(comparator.__dict__, f)
