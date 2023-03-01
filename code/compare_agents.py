fix other bug and add sign in plot

code/compare_agents.py
@@ -286,7 +286,7 @@ class MultipleAgentsComparator:
                 id_reject = np.arange(len(current_decisions))[current_decisions== "continue"][imax]
                 current_decisions[id_reject] = "reject"
                 self.rejected_decision.append(self.current_comparisons[id_reject])
-                current_sign[id_reject].append(2*(Tmaxsigned > 0)-1)
+                current_sign[id_reject] = 2*(Tmaxsigned > 0)-1
                 print("reject")
             elif Tmin < bk_inf:
                 id_accept = np.arange(len(current_decisions))[current_decisions == "continue"][imin]
@@ -479,11 +479,9 @@ class MultipleAgentsComparator:
         links = np.zeros([len(agent_names),len(agent_names)])
         for i in range(len(self.comparisons)):
             c = self.comparisons[i]
-            if self.decisions[i] == "accept":
-                links[c[0],c[1]] = -1
-            else:
-                links[c[0],c[1]] = 1
-        links = links + links.T
+            links[c[0],c[1]] = self.decisions_num[i]
+            
+        links = links + links.T 
         links = links[id_sort,:][:, id_sort]
 
         fig, (ax1, ax2) = plt.subplots(

code/tests/tests.py
@@ -10,13 +10,13 @@ import numpy as np
 B = 5000
 alpha = 0.05
 
-@pytest.mark.parametrize("K,n", [(3, 5), (1, 15), (5, 3)])
+@pytest.mark.parametrize("K,n", [(3, 5), (1, 15)])
 def test_type1(K,n):
     idxs = []
     for M in range(100):
         comparator = MultipleAgentsComparator(n, K,B,  alpha=alpha, seed=M, beta = 0.01, joblib_backend = "multiprocessing")
         agents_evals = [np.hstack([np.random.normal(size=n) for k in range(K)]) for n_agents in range(3)]
-        decision = comparator.compare_scalars(agents_evals)
-        idxs.append(decision == "reject")
+        decisions = comparator.compare_scalars(agents_evals)
+        idxs.append("reject" in decisions)
     assert np.mean(idxs) < 2*alpha, "type 1 error seems to be too large"
         