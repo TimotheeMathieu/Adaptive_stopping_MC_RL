441ad6090814307c88c4aeafd3d93279dd18562c
KohlerHECTOR <hector.kohler@etu.sorbonne-universite.fr>
2023-03-20 13:46:25 +0100

    updated tex

--- code/histograms_n_k.py
+++ code/histograms_n_k.py
@@ -1,14 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#
+# for n in range(2, 6):
+#     for k in range(1, 16):
+#         decisions = np.load("h2g2/decision_n{}_k{}.npy".format(n,k))
+#         comparisons = np.load("h2g2/comparison_n{}_k{}.npy".format(n,k))
+#         hists = []
+#         for i, deci in enumerate(decisions):
+#             if deci:
+#                 hists.append(comparisons[i])
+#         plt.hist(hists)
+#         plt.show()
 
-for n in range(2, 6):
-    for k in range(1, 10):
-        decisions = np.load("h2g2/decision_n{}_k{}.npy".format(n,k))
-        comparisons = np.load("h2g2/comparison_n{}_k{}.npy".format(n,k))
-        hists = []
-        for i, deci in enumerate(decisions):
-            if deci:
-                hists.append(comparisons[i])
-        plt.hist(hists)
-        plt.show()
+decisions = np.load("h2g2/decision_n{}_k{}.npy".format(5,5))
+comparisons = np.load("h2g2/comparison_n{}_k{}.npy".format(5,5))
+hists = []
+for i, deci in enumerate(decisions):
+    if deci:
+        hists.append(comparisons[i])
+plt.hist(hists)
+plt.show()
+
+
+
+l = np.zeros(25)
+
+
+
+for i, compar in enumerate(comparisons):
+    l[int(compar)-1] += 1
+
+plt.bar(np.arange(1,26), l)
+plt.show()
