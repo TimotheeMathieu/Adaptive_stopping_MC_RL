b153253248e040b9d56ed28aad41c9a5121a24b4
mmcenta <matheuscenta@gmail.com>
2023-04-02 16:19:38 +0200

    Update DDPG script

--- experiments2/ant/agents/ddpg.sh
+++ experiments2/ant/agents/ddpg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Seed: $1"
 source activate as_crl
-python ~/adastop/Adaptive_stopping_MC_RL/adastop/deep_rl_agents/ddpg.py -e Ant-v3 -b 1 -s $1
+python ~/Adaptive_stopping_MC_RL/adastop/deep_rl_agents/ddpg.py -e Ant-v3 -b 1 -s $1
